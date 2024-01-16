1e29d2b4d3acee7cf8c7e9f52bd68026b1d10fd0
kedarnathpc <kedarnathpc2002@gmail.com>
2024-01-16 14:15:24 +0530

    assignment updated

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.21.5
 
 require (
 	github.com/disintegration/imaging v1.6.2 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/nurlantulemisov/imagecompression v0.0.0-20211028165702-e399758d3838 // indirect
 	golang.org/x/image v0.15.0 // indirect
 	gonum.org/v1/gonum v0.14.0 // indirect
--- main.go
+++ main.go
@@ -1,77 +1,103 @@
 package main
 
 import (
+	"fmt"
 	"image"
-	"image/png"
+	"image/color"
+	"image/jpeg"
 	"log"
 	"os"
 
-	"github.com/disintegration/imaging"
-	compression "github.com/nurlantulemisov/imagecompression"
+	"github.com/nfnt/resize"
 )
 
 func main() {
-	src, err := imaging.Open("./original/profile.jpg")
-	if err != nil {
-		log.Fatal("Failed to open the image: %v", err)
-	}
-
-	// src = imaging.CropAnchor(src, 300, 300, imaging.Center)
-
-	err = imaging.Save(src, "./compressed/compressed-profile.jpg")
-	if err != nil {
-		log.Fatal("Failed to save the image : %v", err)
-	}
-
-	//create a blurred version of the image
-	img1 := imaging.Blur(src, 25)
-	err = imaging.Save(img1, "./compressed/blurred-profile.jpg")
-	if err != nil {
-		log.Fatal("Failed to save the image : %v", err)
-	}
-
-	//create a grayscale version of the image
-	img2 := imaging.Grayscale(src)
-	err = imaging.Save(img2, "./compressed/grayscale-profile.jpg")
-	if err != nil {
-		log.Fatal("Failed to save the image : %v", err)
-	}
-
-	compress()
-}
-
-func compress() {
 	// Open the image file
-	file, err := os.Open("./original/profile.jpg")
+	file, err := os.Open("./original/input.jpg")
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatal(err)
 	}
 	defer file.Close()
 
 	// Decode the image
 	img, _, err := image.Decode(file)
 	if err != nil {
-		log.Fatal("Error decoding the image:", err)
+		log.Fatal(err)
 	}
 
-	// Compress the image with a compression level
-	compressing, err := compression.New(70)
+	// 1. Resize the image to 512x512 pixels
+	resizedImg := resize.Resize(512, 512, img, resize.Lanczos3)
+
+	// Save the resized image
+	saveImage(resizedImg, "./compressed/resized.jpg")
+
+	// 2. Convert the image to grayscale
+	grayscaleImg := convertToGrayscale(resizedImg)
+
+	// Save the grayscale image
+	saveImage(grayscaleImg, "./compressed/grayscale.jpg")
+
+	// 3. Display separate images for red, blue, and green components
+	redImg := extractColorComponent(resizedImg, color.RGBA{255, 0, 0, 255})
+	blueImg := extractColorComponent(resizedImg, color.RGBA{0, 0, 255, 255})
+	greenImg := extractColorComponent(resizedImg, color.RGBA{0, 255, 0, 255})
+
+	// Save the red, blue, and green images
+	saveImage(redImg, "./compressed/red_component.jpg")
+	saveImage(blueImg, "./compressed/blue_component.jpg")
+	saveImage(greenImg, "./compressed/green_component.jpg")
+
+	fmt.Println("Image processing completed.")
+}
+
+func saveImage(img image.Image, filename string) {
+	file, err := os.Create(filename)
 	if err != nil {
-		log.Fatal("Error creating compression:", err)
+		log.Fatal(err)
 	}
+	defer file.Close()
 
-	compressingImage := compressing.Compress(img)
-
-	// Create the compressed file
-	compressedFile, err := os.Create("./compressed/c70.jpg")
+	err = jpeg.Encode(file, img, nil)
 	if err != nil {
-		log.Fatal("Error creating the compressed file:", err)
+		log.Fatal(err)
 	}
-	defer compressedFile.Close()
+}
 
-	// Encode and save the compressed image
-	err = png.Encode(compressedFile, compressingImage)
-	if err != nil {
-		log.Fatal("Error encoding the compressed image:", err)
+func convertToGrayscale(img image.Image) image.Image {
+	bounds := img.Bounds()
+	gray := image.NewGray(bounds)
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			grayColor := color.GrayModel.Convert(img.At(x, y))
+			gray.Set(x, y, grayColor)
+		}
 	}
+
+	return gray
+}
+
+func extractColorComponent(img image.Image, targetColor color.RGBA) image.Image {
+	bounds := img.Bounds()
+	resultImg := image.NewRGBA(bounds)
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			originalColor := color.NRGBAModel.Convert(img.At(x, y)).(color.NRGBA)
+			resultColor := color.NRGBA{0, 0, 0, originalColor.A}
+
+			switch targetColor {
+			case color.RGBA{255, 0, 0, 255}: // Red component
+				resultColor.R = originalColor.R
+			case color.RGBA{0, 0, 255, 255}: // Blue component
+				resultColor.B = originalColor.B
+			case color.RGBA{0, 255, 0, 255}: // Green component
+				resultColor.G = originalColor.G
+			}
+
+			resultImg.Set(x, y, resultColor)
+		}
+	}
+
+	return resultImg
 }
